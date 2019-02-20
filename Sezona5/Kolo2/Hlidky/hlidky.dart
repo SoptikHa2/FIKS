30c1b80cb0525a4cba246769ef55ff9ef291e5c7
Petr Šťastný <petr.stastny01@gmail.com>
2019-02-20 23:39:31 +0100

    Added first part of choosing path via heap

--- Sezona5/Kolo2/Hlidky/hlidky.dart
+++ Sezona5/Kolo2/Hlidky/hlidky.dart
@@ -55,8 +55,8 @@ class Region {
     var cityFrom = _cities[from];
     var cityTo = _cities[to];
     var path = Path(cityFrom, cityTo, numberOfControls);
-    cityFrom.paths.add(path);
-    cityTo.paths.add(path);
+    cityFrom.paths.insert(path);
+    cityTo.paths.insert(path);
   }
 
   /// Remove old results from all objects here
@@ -85,35 +85,36 @@ class Region {
       Log.writeln("New beginning: " + currentPath.toString());
 
       // Select shortest and 2nd shortest path from this city to another unvisited city
-      // double minDistance = double.infinity;
-      // double minDistance2 = double.infinity;
-      // Path selectedPath = null;
-      // Path secondSelectedPath = null;
-
-      // for (var path in currentPath.cities.last.paths) {
-      //   var anotherCity =
-      //       path.from == currentPath.cities.last ? path.to : path.from;
-      //   if (anotherCity.shortestAvailablePath == null) {
-      //     if (path.distance < minDistance) {
-      //       if (minDistance < minDistance2) {
-      //         minDistance2 = minDistance;
-      //         secondSelectedPath = selectedPath;
-      //       }
-      //       minDistance = path.distance;
-      //       selectedPath = path;
-      //     } else {
-      //       if (path.distance < minDistance2) {
-      //         minDistance2 = path.distance;
-      //         secondSelectedPath = path;
-      //       }
-      //     }
-      //   }
-      // }
-
+      // The shortest one (might) be the one we will go to, in which case the second one will be
+      // added to the list of possible routes from current city
       Path selectedPath = null;
       Path secondSelectedPath = null;
-      double minDistance = selectedPath.distance;
-      double minDistance2 = secondSelectedPath.distance;
+      double minDistance = null;
+      double minDistance2 = null;
+
+      // Choose shortest path
+      while (selectedPath == null) {
+        selectedPath = currentPath.cities.last.paths.findMin().value;
+        if (selectedPath == null) {
+          break;
+        }
+        var cityOnTheOtherEndOfThisPath =
+            selectedPath.from == currentPath.cities.last
+                ? selectedPath.to
+                : selectedPath.from;
+        if (cityOnTheOtherEndOfThisPath.shortestAvailablePath != null) {
+          selectedPath = null;
+          currentPath.cities.last.paths.deleteMin();
+        }
+      }
+      if (selectedPath != null) {
+        minDistance = selectedPath.distance;
+        currentPath.cities.last.paths.deleteMin();
+        // And choose second shortest path
+        while(secondSelectedPath == null){
+          
+        }
+      }
 
       if (selectedPath == null) {
         Log.writeln("No path from last city on path " + currentPath.toString());
@@ -179,7 +180,7 @@ class Region {
     }
     s += "\n\n";
     for (var city in _cities) {
-      for (var path in city.paths) {
+      for (var path in city.paths.toIterable()) {
         if (path.from == city) {
           s +=
               "${path.from} -- ${path.to} [label=${path.distance.toStringAsFixed(0)}]\n";
@@ -203,7 +204,7 @@ class Path {
 
 class City {
   int id;
-  List<Path> paths;
+  PairingMinHeap<Path> paths;
 
   /// null - City was not visited yet
   ///
@@ -213,7 +214,7 @@ class City {
   double shortestAvailablePath = null;
 
   City(this.id) {
-    paths = List<Path>();
+    paths = PairingMinHeap<Path>(null, ((a, b) => a.distance < b.distance));
   }
 
   @override
@@ -281,14 +282,21 @@ class Log {
 /// Tree, where all nodes contain
 /// pointer to left child and
 /// siblings.
-class PairingMinHeap {
+class PairingMinHeap<T> {
   HeapNode root;
+  dynamic defaultMergeFunction;
 
-  PairingMinHeap(this.root);
+  /// root may be null.
+  /// defaultMergeFunction returns `bool` and accepts
+  /// two arguments (`T one`, `T other`).
+  PairingMinHeap(this.root, bool defaultMergeFunction(T one, T other)) {
+    this.defaultMergeFunction = defaultMergeFunction;
+  }
 
   PairingMinHeap.fromHeap(PairingMinHeap first, PairingMinHeap second) {
     this.root = first.root;
     this.root.leftChild = second.root;
+    this.defaultMergeFunction = first.defaultMergeFunction;
   }
 
   /// Return root, it's always the minimum
@@ -311,15 +319,25 @@ class PairingMinHeap {
   }
 
   /// Merge the node with rest of the heap
-  void insert(double value) {
-    this.root = merge(this.root, HeapNode(value));
+  void insert(T value) {
+    this.root = merge(this.root, HeapNode(value, defaultMergeFunction));
+  }
+
+  /// Generate iterable of node values,
+  /// in no particular order.
+  Iterable<T> toIterable() sync* {
+    if (this.root != null) {
+      for (var yieldedValue in root.toIterable()) {
+        yield yieldedValue;
+      }
+    }
   }
 
   /// Merge two heaps
   static HeapNode merge(HeapNode first, HeapNode second) {
     if (first == null) return second;
     if (second == null) return first;
-    if (first.value < second.value) {
+    if (first.compareFunction(first.value, second.value)) {
       if (first.leftChild == null) {
         first.leftChild = second;
       } else {
@@ -337,12 +355,30 @@ class PairingMinHeap {
   }
 }
 
-class HeapNode {
-  double value;
+class HeapNode<T> {
+  T value;
   HeapNode leftChild;
   List<HeapNode> siblings;
+  dynamic compareFunction;
 
-  HeapNode(this.value) {
+  /// Create new heapnode with value of given type T.
+  /// Define compareFunction, that returns if `T one` has
+  /// higher priority (is lower?) than `T other`.
+  HeapNode(this.value, bool compareFunction(T one, T other)) {
     this.siblings = List<HeapNode>();
+    this.compareFunction = compareFunction;
+  }
+
+  /// Generate list of node values
+  Iterable<T> toIterable() sync* {
+    yield this.value;
+    for (var sibling in siblings) {
+      yield sibling.value;
+    }
+    if (leftChild != null) {
+      for (var yieldedValue in leftChild.toIterable()) {
+        yield yieldedValue;
+      }
+    }
   }
 }
